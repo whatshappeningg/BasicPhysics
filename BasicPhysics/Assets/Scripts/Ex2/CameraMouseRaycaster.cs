59d29b5a5b834231433e5ab0f39f28f49fb8a44a
Althea <althea.larrauri@gmail.com>
2026-01-19 17:07:48 +0100

    feat: implementar desactivación de obstáculos clicados

---
+++ BasicPhysics/Assets/Scripts/Ex2/CameraMouseRaycaster.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class CameraMouseRaycaster : MonoBehaviour
+{
+    public Camera camera1;
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = camera1.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                hit.collider.gameObject.SetActive(false);
+            }
+        }
+    }
+}
